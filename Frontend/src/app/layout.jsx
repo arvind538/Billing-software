65d5096394e548dc2726278a31d75742e653d497
Arvind <your-email@example.com>
2026-08-17 16:13:29 +0530

    first commit

--- Frontend/src/app/layout.jsx
+++ Frontend/src/app/layout.jsx
@@ -34,7 +34,7 @@ export default function RootLayout({ children }) {
         </CartProvider>
         <ToastContainer
           position="top-right"
-          autoClose={5000}
+          autoClose={3000}
           hideProgressBar={false}
           newestOnTop={false}
           closeOnClick={false}
